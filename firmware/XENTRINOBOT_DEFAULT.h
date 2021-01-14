98343dfcda47f1cb8c9104e628b4a91a9a6088db
Christopher Coballes <cobecoballes@gmail.com>
2021-01-14 09:40:45 -0500

    Update XENTRINOBOT_DEFAULT.h

--- firmware/XENTRINOBOT_DEFAULT.h
+++ firmware/XENTRINOBOT_DEFAULT.h
@@ -16,3 +16,14 @@ const double encoder_cpr = 990;               //Encoder ticks or counts per rota
 const double speed_to_pwm_ratio = 0.00235;    //Ratio to convert speed (in m/s) to PWM value. It was obtained by plotting the wheel speed in relation to the PWM motor command (the value is the slope of the linear function).
 const double min_speed_cmd = 0.0882;          //(min_speed_cmd/speed_to_pwm_ratio) is the minimum command value needed for the motor to start moving. This value was obtained by plotting the wheel speed in relation to the PWM motor command (the value is the constant of the linear function).
 
+#define PID
+double speed_req = 0;                         //Desired linear speed for the robot, in m/s
+double angular_speed_req = 0;                 //Desired angular speed for the robot, in rad/s
+
+double speed_req_left = 0;                    //Desired speed for left wheel in m/s
+double speed_act_left = 0;                    //Actual speed for left wheel in m/s
+double speed_cmd_left = 0;                    //Command speed for left wheel in m/s 
+
+double speed_req_right = 0;                   //Desired speed for right wheel in m/s
+double speed_act_right = 0;                   //Actual speed for right wheel in m/s
+double speed_cmd_right = 0;                   //Command speed for right wheel in m/s 
