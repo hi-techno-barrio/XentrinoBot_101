e8bb1a0ddd23e363941ec104c1859e0068f2c1b0
Christopher Coballes <cobecoballes@gmail.com>
2021-01-14 09:37:50 -0500

    Update XENTRINOBOT_DEFAULT.h

--- firmware/XENTRINOBOT_DEFAULT.h
+++ firmware/XENTRINOBOT_DEFAULT.h
@@ -1,1 +1,8 @@
 
+// constant for encoder
+
+const int PIN_ENCOD_A_MOTOR_LEFT = 2;               //A channel for encoder of left motor                    
+const int PIN_ENCOD_B_MOTOR_LEFT = 4;               //B channel for encoder of left motor
+
+const int PIN_ENCOD_A_MOTOR_RIGHT = 3;              //A channel for encoder of right motor         
+const int PIN_ENCOD_B_MOTOR_RIGHT = 5;              //B channel for encoder of right motor 
