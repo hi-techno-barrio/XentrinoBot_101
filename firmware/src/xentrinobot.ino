316920a000a7331435dd4aafbd6cddeba742a9bb
Christopher Coballes <cobecoballes@gmail.com>
2021-01-14 10:00:43 -0500

    Update xentrinobot.ino

--- firmware/src/xentrinobot.ino
+++ firmware/src/xentrinobot.ino
@@ -14,52 +14,17 @@ unsigned int noCommLoops = 0;                 //main loop without communication 
 
 double speed_cmd_left2 = 0;      
 
-const int PIN_ENCOD_A_MOTOR_LEFT = 2;               //A channel for encoder of left motor                    
-const int PIN_ENCOD_B_MOTOR_LEFT = 4;               //B channel for encoder of left motor
-
-const int PIN_ENCOD_A_MOTOR_RIGHT = 3;              //A channel for encoder of right motor         
-const int PIN_ENCOD_B_MOTOR_RIGHT = 5;              //B channel for encoder of right motor 
+// #define encoder
 
 const int PIN_SIDE_LIGHT_LED = 46;                  //Side light blinking led pin
 
 unsigned long lastMilli = 0;
 
-//--- Robot-specific constants ---
-const double radius = 0.04;                   //Wheel radius, in m
-const double wheelbase = 0.187;               //Wheelbase, in m
-const double encoder_cpr = 990;               //Encoder ticks or counts per rotation
-const double speed_to_pwm_ratio = 0.00235;    //Ratio to convert speed (in m/s) to PWM value. It was obtained by plotting the wheel speed in relation to the PWM motor command (the value is the slope of the linear function).
-const double min_speed_cmd = 0.0882;          //(min_speed_cmd/speed_to_pwm_ratio) is the minimum command value needed for the motor to start moving. This value was obtained by plotting the wheel speed in relation to the PWM motor command (the value is the constant of the linear function).
+#define motor 
 
-double speed_req = 0;                         //Desired linear speed for the robot, in m/s
-double angular_speed_req = 0;                 //Desired angular speed for the robot, in rad/s
 
-double speed_req_left = 0;                    //Desired speed for left wheel in m/s
-double speed_act_left = 0;                    //Actual speed for left wheel in m/s
-double speed_cmd_left = 0;                    //Command speed for left wheel in m/s 
+#define PID
 
-double speed_req_right = 0;                   //Desired speed for right wheel in m/s
-double speed_act_right = 0;                   //Actual speed for right wheel in m/s
-double speed_cmd_right = 0;                   //Command speed for right wheel in m/s 
-                        
-const double max_speed = 0.4;                 //Max speed in m/s
-
-int PWM_leftMotor = 0;                     //PWM command for left motor
-int PWM_rightMotor = 0;                    //PWM command for right motor 
-                                                      
-// PID Parameters
-const double PID_left_param[] = { 0, 0, 0.1 }; //Respectively Kp, Ki and Kd for left motor PID
-const double PID_right_param[] = { 0, 0, 0.1 }; //Respectively Kp, Ki and Kd for right motor PID
-
-volatile float pos_left = 0;       //Left motor encoder position
-volatile float pos_right = 0;      //Right motor encoder position
-
-PID PID_leftMotor(&speed_act_left, &speed_cmd_left, &speed_req_left, PID_left_param[0], PID_left_param[1], PID_left_param[2], DIRECT);          //Setting up the PID for left motor
-PID PID_rightMotor(&speed_act_right, &speed_cmd_right, &speed_req_right, PID_right_param[0], PID_right_param[1], PID_right_param[2], DIRECT);   //Setting up the PID for right motor
-
-Adafruit_MotorShield AFMS = Adafruit_MotorShield();  // Create the motor shield object with the default I2C address
-Adafruit_DCMotor *leftMotor = AFMS.getMotor(1);      //Create left motor object
-Adafruit_DCMotor *rightMotor = AFMS.getMotor(2);     //Create right motor object
   
 ros::NodeHandle nh;
 
@@ -79,112 +44,82 @@ ros::Subscriber<geometry_msgs::Twist> cmd_vel("cmd_vel", handle_cmd);   //create
 geometry_msgs::Vector3Stamped speed_msg;                                //create a "speed_msg" ROS message
 ros::Publisher speed_pub("speed", &speed_msg);                          //create a publisher to ROS topic "speed" using the "speed_msg" type
 
-const int lightIncNumber = 30;                                                                                                                                       //Number of lightIncrements for side light blinking
-int lightInc = 0;                                                                                                                                                    //Init increment for side light blinking
-int lightValue [lightIncNumber]= { 10, 40, 80, 160, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 160, 80, 40, 10, 0, 0, 0, 0, 0, 0, 0, 0 }; //side light increment values
-int lightValueNoComm [25]= { 255, 0, 255, 0, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }; //side light increment values
-int lightT = 0; //init light period
-
 //__________________________________________________________________________
 
 void setup() {
 
-  
-  pinMode(PIN_SIDE_LIGHT_LED, OUTPUT);      //set pin for side light leds as output
-  analogWrite(PIN_SIDE_LIGHT_LED, 255);     //light up side lights
-  
   nh.initNode();                            //init ROS node
   nh.getHardware()->setBaud(57600);         //set baud for ROS serial communication
   nh.subscribe(cmd_vel);                    //suscribe to ROS topic for velocity commands
   nh.advertise(speed_pub);                  //prepare to publish speed in ROS topic
  
-  AFMS.begin();
-  
+   //initialize motor
+   
   //setting motor speeds to zero
-  leftMotor->setSpeed(0);
-  leftMotor->run(BRAKE);
-  rightMotor->setSpeed(0);
-  rightMotor->run(BRAKE);
  
   //setting PID parameters
-  PID_leftMotor.SetSampleTime(95);
-  PID_rightMotor.SetSampleTime(95);
-  PID_leftMotor.SetOutputLimits(-max_speed, max_speed);
-  PID_rightMotor.SetOutputLimits(-max_speed, max_speed);
-  PID_leftMotor.SetMode(AUTOMATIC);
-  PID_rightMotor.SetMode(AUTOMATIC);
-    
   // Define the rotary encoder for left motor
-  pinMode(PIN_ENCOD_A_MOTOR_LEFT, INPUT); 
-  pinMode(PIN_ENCOD_B_MOTOR_LEFT, INPUT); 
-  digitalWrite(PIN_ENCOD_A_MOTOR_LEFT, HIGH);                // turn on pullup resistor
-  digitalWrite(PIN_ENCOD_B_MOTOR_LEFT, HIGH);
-  attachInterrupt(0, encoderLeftMotor, RISING);
+   //attachInterrupt(0, encoderLeftMotor, RISING);
 
   // Define the rotary encoder for right motor
-  pinMode(PIN_ENCOD_A_MOTOR_RIGHT, INPUT); 
-  pinMode(PIN_ENCOD_B_MOTOR_RIGHT, INPUT); 
-  digitalWrite(PIN_ENCOD_A_MOTOR_RIGHT, HIGH);                // turn on pullup resistor
-  digitalWrite(PIN_ENCOD_B_MOTOR_RIGHT, HIGH);
-  attachInterrupt(1, encoderRightMotor, RISING);
+  //attachInterrupt(1, encoderRightMotor, RISING);
 }
 
 //_________________________________________________________________________
 
 void loop() {
   nh.spinOnce();
-  if((millis()-lastMilli) >= LOOPTIME)   
+ // algorithm 1
+ if((millis()-lastMilli) >= LOOPTIME)   
   {                                                                           // enter timed loop
     lastMilli = millis();
     
     if (!nh.connected()){
-      analogWrite(PIN_SIDE_LIGHT_LED, lightValueNoComm[lightInc]);
-      lightInc=lightInc+1;
-      if (lightInc >= 25){
-        lightInc=0;
-      }
-    }
-    else{
-      analogWrite(PIN_SIDE_LIGHT_LED, lightValue [lightInc]);
-      lightT = 3000 - ((2625/max_speed)*((abs(speed_req_left)+abs(speed_req_right))/2));
-      lightInc=lightInc+(30/(lightT/LOOPTIME));
-      if (lightInc >= lightIncNumber){
-        lightInc=0;
-      }
-    }
+    buzzer  
+        }
+     }
     
+  else{
+    // alarm
+     }
     
+  //algo 1  detect disturbance   
     if (abs(pos_left) < 5){                                                   //Avoid taking in account small disturbances
       speed_act_left = 0;
     }
+ 
     else {
-      speed_act_left=((pos_left/encoder_cpr)*2*PI)*(1000/LOOPTIME)*radius;           // calculate speed of left wheel
+     // get motor speed 1
+       speed_act_left=((pos_left/encoder_cpr)*2*PI)*(1000/LOOPTIME)*radius;           // calculate speed of left wheel
     }
     
+ // algo 1 detect disturbance
     if (abs(pos_right) < 5){                                                  //Avoid taking in account small disturbances
       speed_act_right = 0;
     }
     else {
-    speed_act_right=((pos_right/encoder_cpr)*2*PI)*(1000/LOOPTIME)*radius;          // calculate speed of right wheel
+    // get motor speed 2
+    //speed_act_right=((pos_right/encoder_cpr)*2*PI)*(1000/LOOPTIME)*radius;          // calculate speed of right wheel
     }
     
     pos_left = 0;
     pos_right = 0;
-
-    speed_cmd_left = constrain(speed_cmd_left, -max_speed, max_speed);
-    PID_leftMotor.Compute();                                                 
+     //algo 3 invoke speed boundaries
+     speed_cmd_left = constrain(speed_cmd_left, -max_speed, max_speed);
+     PID_leftMotor.Compute();                                                 
     // compute PWM value for left motor. Check constant definition comments for more information.
     PWM_leftMotor = constrain(((speed_req_left+sgn(speed_req_left)*min_speed_cmd)/speed_to_pwm_ratio) + (speed_cmd_left/speed_to_pwm_ratio), -255, 255); //
     
-    if (noCommLoops >= noCommLoopMax) {                   //Stopping if too much time without command
+    // algo 2 check idle command  
+   if ( (noCommLoops >= noCommLoopMax) || (speed_req_left == 0) )  {     //Stopping if too much time without command
+                          //Stopping
       leftMotor->setSpeed(0);
       leftMotor->run(BRAKE);
     }
-    else if (speed_req_left == 0){                        //Stopping
-      leftMotor->setSpeed(0);
-      leftMotor->run(BRAKE);
+    else {
+     motor.spin() // if actual pwm is positive move forward else move backward
     }
-    else if (PWM_leftMotor > 0){                          //Going forward
+     /*if (PWM_leftMotor > 0){                          //Going forward
       leftMotor->setSpeed(abs(PWM_leftMotor));
       leftMotor->run(BACKWARD);
     }
@@ -192,6 +127,7 @@ void loop() {
       leftMotor->setSpeed(abs(PWM_leftMotor));
       leftMotor->run(FORWARD);
     }
+    */
     
     speed_cmd_right = constrain(speed_cmd_right, -max_speed, max_speed);    
     PID_rightMotor.Compute();                                                 
