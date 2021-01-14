3ed341591c0f5db5f507d3f7025de7d8c06d6f2b
Christopher Coballes <cobecoballes@gmail.com>
2021-01-14 09:39:01 -0500

    Update XENTRINOBOT_DEFAULT.h

--- firmware/XENTRINOBOT_DEFAULT.h
+++ firmware/XENTRINOBOT_DEFAULT.h
@@ -6,3 +6,13 @@ const int PIN_ENCOD_B_MOTOR_LEFT = 4;               //B channel for encoder of l
 
 const int PIN_ENCOD_A_MOTOR_RIGHT = 3;              //A channel for encoder of right motor         
 const int PIN_ENCOD_B_MOTOR_RIGHT = 5;              //B channel for encoder of right motor 
+
+#define robot
+
+//--- Robot-specific constants ---
+const double radius = 0.04;                   //Wheel radius, in m
+const double wheelbase = 0.187;               //Wheelbase, in m
+const double encoder_cpr = 990;               //Encoder ticks or counts per rotation
+const double speed_to_pwm_ratio = 0.00235;    //Ratio to convert speed (in m/s) to PWM value. It was obtained by plotting the wheel speed in relation to the PWM motor command (the value is the slope of the linear function).
+const double min_speed_cmd = 0.0882;          //(min_speed_cmd/speed_to_pwm_ratio) is the minimum command value needed for the motor to start moving. This value was obtained by plotting the wheel speed in relation to the PWM motor command (the value is the constant of the linear function).
+
