e23b665becbe8bc9528eb5d1c4836ea2afb6c56c
Christopher Coballes <cobecoballes@gmail.com>
2021-01-21 10:12:22 +0800

    Update kalman.h

--- firmware/lib/kalman/kalman.h
+++ firmware/lib/kalman/kalman.h
@@ -1,57 +1,27 @@
 
+#ifndef KalmanFilter_h
+#define KalmanFilter_h
+
 #if ARDUINO >= 100
 #include "Arduino.h"
 #else
 #include "WProgram.h"
 #endif
 
-#include "KalmanFilter.h"
-
-KalmanFilter::KalmanFilter(double angle, double bias, double measure)
+class KalmanFilter
 {
-    Q_angle = angle;
-    Q_bias = bias;
-    R_measure = measure;
+    public:
 
-    K_angle = 0;
-    K_bias = 0;
+	KalmanFilter(double angle = 0.001, double bias = 0.003, double measure = 0.03);
+	double update(double newValue, double newRate);
 
-    P[0][0] = 0;
-    P[0][1] = 0;
-    P[1][0] = 0;
-    P[1][1] = 0;
+    private:
 
-    kt = (double)micros();
-}
-
-double KalmanFilter::update(double newValue, double newRate)
-{
-    dt = (double)(micros() - kt) / 1000000;
-
-    K_rate = newRate - K_bias;
-    K_angle += dt * K_rate;
-
-    P[0][0] += dt * (P[1][1] + P[0][1]) + Q_angle * dt;
-    P[0][1] -= dt * P[1][1];
-    P[1][0] -= dt * P[1][1];
-    P[1][1] += Q_bias * dt;
-
-    S = P[0][0] + R_measure;
-
-    K[0] = P[0][0] / S;
-    K[1] = P[1][0] / S;
-
-    y = newValue - K_angle;
-
-    K_angle += K[0] * y;
-    K_bias += K[1] * y;
-
-    P[0][0] -= K[0] * P[0][0];
-    P[0][1] -= K[0] * P[0][1];
-    P[1][0] -= K[1] * P[0][0];
-    P[1][1] -= K[1] * P[0][1];
-
-    kt = (double)micros();
-
-    return K_angle;
+	double Q_angle, Q_bias, R_measure;
+	double K_angle, K_bias, K_rate;
+	double P[2][2], K[2];
+	double S, y;
+	double dt, kt;
 };
+
+#endif
